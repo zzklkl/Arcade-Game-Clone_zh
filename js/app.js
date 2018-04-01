32d040fe624f44b3f5f75f00dc436a870f404be7
周凯磊 <kl@zhoukaileideMacBook-Pro.local>
2018-04-01 22:09:21 +0800

    完成基本功能

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
+// 定义 dom
+var success = document.querySelector('.success');
+var restartBtn = document.querySelector('.restart');
+
+// 定义全局变量
+var playerX = 300;
+var playerY = 391;
+
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-    this.x = 505 * Math.random();
-    this.y = 83 * (Math.floor(Math.random() * 3) + 1) - 24;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
@@ -13,7 +22,11 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
-
+    this.x += this.speed * dt;
+    if(this.x > 550) {
+        this.x = -100;
+        this.y = 83 * (Math.floor(Math.random() * 3) + 1) - 24;
+    }
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -24,12 +37,24 @@ Enemy.prototype.render = function() {
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 var Player = function () {
-    this.x = 300;
-    this.y = 388;
+    this.x = playerX;
+    this.y = playerY;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function() {
+    if(this.x > 404) {
+        this.x = 404;
+    }
+    if(this.x < 0) {
+        this.x = 0;
+    }
+    if(this.y > playerY) {
+        this.y = playerY;
+    }
+    if(this.y < 0) {
+        gameSuccess();
+    }
 
 };
 
@@ -51,17 +76,24 @@ Player.prototype.handleInput = function(keycode) {
         case 'down':
             this.y += 83;
             break;
-    }
+    }  
 };
+Player.prototype.reset = function() {
+    this.x = playerX;
+    this.y = playerY;
+} 
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 var allEnemies = [];
 var enemies = 3;
+
 for(var i = 0; i < enemies; i++){
-    allEnemies.push(new Enemy());
+    var x = -100 + Math.random() * (505-0);
+    var y = 83 * (Math.floor(Math.random() * 3) + 1) - 24;
+    var speed =  50 + Math.floor(50 * Math.random());
+    allEnemies.push(new Enemy(x, y, speed));
 }
-console.log(allEnemies);
 // 把玩家对象放进一个叫 player 的变量里面
 var player = new Player();
 
@@ -78,3 +110,14 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+restartBtn.addEventListener('click', function() {
+    success.style.display = 'none';
+    
+
+})
+
+function gameSuccess() {
+    success.style.display = 'block';
+    player.reset();
+}
--- js/engine.js
+++ js/engine.js
@@ -64,7 +64,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* 这个函数会遍历在 app.js 定义的存放所有敌人实例的数组，并且调用他们的 update()
@@ -76,6 +76,19 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+    }
+
+    /**
+     * checkCollisions 碰撞检测
+     * 
+     */
+    function checkCollisions() {
+        allEnemies.forEach(function(enemy) {
+            if(enemy.y === player.y && Math.abs(player.x - enemy.x) < 80){
+                player.reset();
+               
+            };
+        });
     }
 
     /* 这个函数做了一些游戏的初始渲染，然后调用 renderEntities 函数。记住，这个函数
